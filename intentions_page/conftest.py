47d77a616e048e759d85bd3291e45095485b9e45
tadamcz <tmkadamcz@gmail.com>
2021-04-25 21:34:46 +0100

    initial commit
    created with cookiecutter-django
    
    tested to work using:
    `manage.py runserver localhost:8000`

---
+++ intentions_page/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from intentions_page.users.models import User
+from intentions_page.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
