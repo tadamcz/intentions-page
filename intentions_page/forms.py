c739a432d738c7dcad5404e0d63ed33b05c7daed
tadamcz <tmkadamcz@gmail.com>
2021-08-09 13:02:50 +0100

    clarifying comments

--- intentions_page/forms.py
+++ intentions_page/forms.py
@@ -12,6 +12,10 @@ class IntentionEditForm(forms.ModelForm):
 class IntentionsDraftEditForm(forms.ModelForm):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+
+		# This sizing code is later overridden by js autosize
+		# but it's a good idea to have something approximately correct already in the HTML before the JS comes in.
+		# The code here will produce the same height as JS if all intentions take up one line or less.
 		if self.instance.content:
 			rows = len(self.instance.content.splitlines())
 		else:
@@ -32,6 +36,9 @@ class NoteEditForm(forms.ModelForm):
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+
+		# For notes, we can't make very good assumptions for the number of rows that autosize will pick,
+		# so we use a constant number of rows
 		self.fields['content'].widget.attrs = {'rows': 2}
 
 	helper = FormHelper()
